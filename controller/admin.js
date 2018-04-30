861ac91ea7c71e30c8cd6bb6a8d8f0ce943c9ee6
husida <443827004@qq.com>
2018-05-01 01:56:38 +0800

    login

--- controller/admin.js
+++ controller/admin.js
@@ -9,9 +9,48 @@ class Admin{
     }
 
     async regist(req, res, next) {
-        AdminModel.create({"username":"sss","password":"password"}, () =>{
-            res.json({msg:'成功'});
-        });
+        const {userName, password, status} = req.body;
+
+        if(userName === '') {
+            res.send({
+                code: 0,
+                msg: '用户名不能为空'
+            })
+        } else if (password === '') {
+            res.send({
+                code: 0,
+                msg: '密码不能为空'
+            })
+        }
+
+        AdminModel.findOne({'userName':userName}, (err, admin) => {
+          
+            if(admin){
+                res.send({
+                    code: 0,
+                    msg: '该用户已存在',
+                })
+            } else {
+                AdminModel.create({"userName": userName,"password":password, status:status}, (err) =>{
+                    if (err) {
+                        res.send({
+                            code: 0,
+                            msg: '注册用户失败',
+                        })
+                    } else {
+                        res.send({
+                            code: 1,
+                            msg: '注册成功',
+                        })
+                    }
+                   
+                });
+                
+            }
+        })
+       
+        
+       
     }
 }
 
--- models/admin.js
+++ models/admin.js
@@ -2,18 +2,28 @@
 
 const mongoose = require('mongoose');
 const MongooseDao = require('../dao/MongooseDao')
+const dayjs = require('dayjs');
+const uuidv1 = require('uuid/v1');
 
 const Schema =mongoose.Schema;
 
 const adminSchema = new Schema({
-	name: String,
-	password: String,
-	id: Number,
-	createTime: String,
-	admin: {type: String, default: '管理员'},
-	status: Number,  //1:超级管理员  2:普通管理员 
+	userName: {type: String, required: true},
+	password: {type: String, required: true},
+	id:{type: String, default: uuidv1()},
+	createTime: {type: String, default:dayjs().format('YYYY-MM-DD HH:mm:ss')},
+	status: {type: Number, default: 4},  //1:超级管理员  2:普通管理员 3:会员 4:普通用户
 	avatar: {type: String, default: 'default.jpg'}
 })
+
+// 用户是否存在
+adminSchema.methods.isExist = function(cb) {
+	var query;
+	query = {
+		userName: this.userName
+	};
+	return this.model('admin').findOne(query, cb);
+};
 
 const adminModel = mongoose.model('admin', adminSchema);
 
